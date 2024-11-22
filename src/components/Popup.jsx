7245b3d4f55f28ca5c87147ba01415678bb1e317
Ruthgyeul <wingd4705@gmail.com>
2024-11-22 11:55:01 +0900

    Add popup and dashboard page

---
+++ src/components/Popup.jsx
@@ -0,0 +1,12 @@
+import style from '../styles/modules/Popup.module.css';
+
+function Popup({ infoJson }) {
+    return (
+        <div className={style.Popup}>
+            <div className={style.box}>
+            </div>
+        </div>
+    );
+}
+
+export default Popup;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from '../styles/modules/Dashboard.module.css';
+
+function Dashboard() {
+    return (
+        <div className={style.Dashboard}>
+        </div>
+    );
+}
+
+export default Dashboard;
